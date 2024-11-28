d895c9340bae28ab2098d14252687cd78374acce
carlosj-arague <carlosj.arague@gmail.com>
2024-11-28 18:50:12 +0000

    Implementacion Ayudas v3

--- backend/services/items.js
+++ backend/services/items.js
@@ -39,14 +39,49 @@ async function deleteData(req, res) {
     const data = req.query
     const result = await db.query(
         `DELETE FROM coleccion WHERE id=${data.id}`
+
     )
     /*En la variable result se almacena lo que devueve la consulta. Si accedemos a effectedRow nos da el número de filas de la base de
     datos que ha sido borrado. Si ese número es mayor que cero es que ha habido borrado en la base de datos.*/
     return result.affectedRows
 }
+
+
+
+//Función con la consulta para borrar datos de la base de datos: DELETE
+async function getUsers(req, res) {
+    //La variable rows almacena los datos obtenidos de la consulta select.
+    const rows = await db.query(
+        `SELECT * FROM usuarios`
+    )
+
+    /*Los datos obtenidos de la consulta del select los paso por la función helper para que
+    en el caso de que no haya datos devueltos, me devuelva un array vacío. */
+    const data = helper.emptyOrRows(rows)
+    return {
+        //Devolvemos el resultado del Select, que está almacenado en la variable data
+        data
+    }
+}
+
+async function insertUser(req, res) {
+    //data tiene los datos que vamos a insertar en la base de datos. Los coge de req.query
+    //Para acceder a cada uno de los datos: data.nombre, data.precio, ...
+    const data = req.query
+    const result = await db.query( `INSERT INTO usuarios(nombre, login, password, rol)
+    VALUES ('${data.nombre}', '${data.login}', '${data.password}', '${data.rol}' )`
+    
+);
+    /*En la variable result se almacena lo que devueve la consulta. Si accedemos a effectedRow nos da el número de filas de la base de
+    datos que ha sido modificado o añadido. Si ese número es mayor que cero es que ha habido inserción en la base de datos.*/
+    return result.affectedRows
+}
+
 //Al final del fichero exporto las funciones getData, insertData y deleteData
 module.exports = {
     getData,
     insertData,
-    deleteData
+    deleteData,
+    getUsers,
+    insertUser
 }
